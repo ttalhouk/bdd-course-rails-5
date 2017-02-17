5c3dfce6f50d57e4d31fd7b09d496acc6b45210a
Talal Talhouk <talal@sbcglobal.net>
2017-02-17 10:30:47 -0800

    update edit article spec

--- spec/features/edit_article_spec.rb
+++ spec/features/edit_article_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Editing an article" do
     @user = User.create!({email:"example@example.com", password:"123456", password_confirmation:"123456"})
 
     login_as(@user)
-    @article = @user.article.create(title:'The First Article', body:'Lorem Ipsum Carpe Diem')
+    @article = @user.articles.create(title:'The First Article', body:'Lorem Ipsum Carpe Diem')
   end
 
   scenario "A user edits article details" do
